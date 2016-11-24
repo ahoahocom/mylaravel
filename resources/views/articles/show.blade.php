06a50b5d9a9e59613e1e0cc28e5399e44c3523f9
sn1142 <ahoaho.com12@gmail.com>
2016-11-24 18:08:47 +0900

    1124

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -8,4 +8,13 @@
   <article>
     <div class="body">{{$article->body}}</div>
   </article>
+
+  <br/>
+
+  {!! link_to(action('ArticlesController@edit', [$article->id]), '編集', ['class' => 'btn btn-primary']) !!}
+
+  <br/>
+  <br/>
+
+  {!! delete_form(['articles', $article->id]) !!}
 @endsection
